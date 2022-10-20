a8f34a09dc471f1965be8294bd06c408a7cf2f7d
Smash2222 <karbovskyi.volodymyr@gmail.com>
2022-10-20 17:52:10 +0300

    add p-files(p-admin.php, p-read.php, p-signup.php, p-write.php) which processing basic .php

--- phpproject/includes/process/p-signup.php
+++ phpproject/includes/process/p-signup.php
@@ -1,2 +1,30 @@
 <?php
 
+$h = new Helper();
+$msg = '';
+$username = '';
+
+if ($_POST['submit']) {
+    $username = $_POST['username'];
+
+    if ($h->isEmpty(array($username, $_POST['password'], $_POST['confirm_password']))) {
+        $msg = 'All fields are required';
+    } elseif (!$h->isValidLength($username, 6, 100)) {
+        $msg = 'Username must be between 6 and 100 characters';
+    } elseif (!$h->isValidLength($_POST['password'])) {
+        $msg = 'Password must be between 8 and 20 characters';
+    } elseif (!$h->isSecure($_POST['password'])) {
+        $msg = 'Password must contain at least one lowercase character, one uppercase character and one digit';
+    } elseif (!$h->passwordsMatch($_POST['password'], $_POST['confirm_password'])) {
+        $msg = 'Passwords do not match';
+    } else {
+        $member = new BlogMember($username);
+
+        if ($member->isDuplicateID()) {
+            $msg = "Username already in use";
+        } else {
+            $member->insertIntoMemberDB($_POST['password']);
+            header("Location: index.php?new=1");
+        }
+    }
+}
--- phpproject/includes/process/p-write.php
+++ phpproject/includes/process/p-write.php
@@ -1,2 +1,26 @@
 <?php
 
+if (isset($_SESSION['username'], $_SESSION['is_admin'])) {
+    header("Location: admin.php");
+} else {
+    $h = new Helper();
+    $msg = '';
+    $title = '';
+    $post = '';
+    $audience = '';
+
+
+    if (isset($_POST['submit'])) {
+        $title = $_POST['title'];
+        $post = $_POST['post'];
+        $audience = $_POST['audience'];
+
+        if ($h->isEmpty(array($title, $post, $audience))) {
+            $msg = 'All fields are required';
+        } else {
+            $admin = new Admin($_SESSION['username']);
+            $admin->insertIntoPostDB($title, $post, $audience);
+            $msg = 'Message saved successfully';
+        }
+    }
+}
